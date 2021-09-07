1bfc81e38226a618bf36fd9e92442f708285b5b2
sakshisongat <sakshisongat333@gmail.com>
2021-09-07 13:26:17 +0530

    mongoose atlas connection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,14 +1,23 @@
 // mongoose
 const mongoose = require ("mongoose");
-mongoose.connect(process.env.MONGODB_URL||"mongodb://localhost/databaseName", {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-  })
-  .then(() => {
+const DB = 'mongodb+srv://sakshisongat:2face3book@cluster0.rvukv.mongodb.net/systemdata?retryWrites=true&w=majority'
+mongoose.connect(DB,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+}).then(() => {
     console.log("connected to mongodb cloud! :)");
-  })
-  .catch((err) => {
+  }).catch((err) => {
     console.log(err);
   });
+
+
+// mongoose.connect(process.env.MONGODB_URL||"mongodb://localhost/databaseName", {
+//   })
+//   .then(() => {
+//     console.log("connected to mongodb cloud! :)");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
