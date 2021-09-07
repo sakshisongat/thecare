f1a3dc348a5712224a5720b475e4261e99e89fe9
sakshisongat <sakshisongat333@gmail.com>
2021-09-07 12:01:04 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -116,7 +116,17 @@ app
       })
       .catch((err) => console.log(err));
   });
-
+  
+  // auth 
+  module.exports = (req, res, next) => {
+    if (!req.session.user) {
+      res.send("You're not allowed to view this content! please log in first!");
+      return;
+    }
+    //else continue
+    next();
+  };
+  
 //logout
 app.get("/logout", authenticateUser, (req, res) => {
   req.session.user = null;
